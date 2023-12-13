a8e454b3088b0893d8533f7792541b048354f789
phalcin <phalcinhayes@gmail.com>
2023-12-13 15:26:04 +0000

    we move

--- app.js
+++ app.js
@@ -1,32 +1,12 @@
-// window.firstName = 'Emmanuel';
-// this.lastName = 'Mensah';
-// console.log(window);
-
-// function printThis() {
-//   return this;
-// }
-
-// const res = printThis();
-// console.log(res);
-
-const obj = {
-  firstName: 'Huxn',
-  lastName: 'Webdev',
-  fullName: function () {
-    return `${this.firstName} ${this.lastName}`;
+const person = {
+  name: 'Emmanuel',
+  age: 20,
+  greetRegular: function () {
+    console.log(`Hello my name is ${this.name} & I'm ${this.age} years old`);
+  },
+  greetArrow: () => {
+    console.log(`Hello my name is ${this.name} & I'm ${this.age} years old`);
   },
 };
 
-const res = obj.fullName();
-console.log(res);
-
-const obj = {
-  firstName: 'Huxn',
-  lastName: 'Webdev',
-  fullName: function () {
-    return `${this.firstName} ${this.lastName}`;
-  },
-};
-
-const res = obj.fullName();
-console.log(res);
+console.log(person.greetRegular(), person.greetArrow());
