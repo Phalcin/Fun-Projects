7281fe95beeadde28d5057d7d4b1a453266a9057
phalcin <phalcinhayes@gmail.com>
2023-12-14 15:28:22 +0000

    new changes

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
-const person = {
-  name: 'Emmanuel',
-  age: 20,
-  greetRegular: function () {
-    console.log(`Hello my name is ${this.name} & I'm ${this.age} years old`);
-  },
-  greetArrow: () => {
-    console.log(`Hello my name is ${this.name} & I'm ${this.age} years old`);
+let person = {
+  greet: function () {
+    console.log(`Hello my name is ${this.firstName} ${this.lastName}`);
   },
 };
 
-console.log(person.greetRegular(), person.greetArrow());
-console.log('well');
+const huxn = Object.create(person);
+
+huxn.firstName = 'Huxn';
+huxn.lastName = 'Webdev';
+huxn.greet();
