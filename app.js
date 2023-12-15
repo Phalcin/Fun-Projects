60acb30065c287f16eb100c6c90a3c6cb86d6204
phalcin <phalcinhayes@gmail.com>
2023-12-15 21:20:14 +0000

    new changes

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 const people = [
-  { name: 'bob', age: 20, position: 'developer' },
-  { name: 'peter', age: 25, position: 'designer' },
-  { name: 'suzy', age: 30, position: 'the boss' },
+  { name: 'bob', age: 20, position: 'developer', id: 1 },
+  { name: 'peter', age: 25, position: 'designer', id: 2 },
+  { name: 'suzy', age: 30, position: 'the boss', id: 3 },
+  { name: 'anna', age: 35, position: 'the boss', id: 4 },
 ];
+
+const youngPerson = people.find((person) => {
+  return person.id === 3;
+});
+
+console.log(youngPerson);
