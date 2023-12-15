0f3e5d2c5911818d020ea59523ea422bdd82d702
phalcin <phalcinhayes@gmail.com>
2023-12-15 21:31:25 +0000

    new  changes

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const people = [
-  { name: 'bob', age: 20, position: 'developer', id: 1 },
-  { name: 'peter', age: 25, position: 'designer', id: 2 },
-  { name: 'suzy', age: 30, position: 'the boss', id: 3 },
-  { name: 'anna', age: 35, position: 'the boss', id: 4 },
+  { name: 'bob', age: 20, position: 'developer', id: 1, salary: 200 },
+  { name: 'peter', age: 25, position: 'designer', id: 2, salary: 300 },
+  { name: 'suzy', age: 30, position: 'the boss', id: 3, salary: 500 },
+  { name: 'anna', age: 35, position: 'the boss', id: 4, salary: 500 },
 ];
 
-const youngPerson = people.find((person) => {
-  return person.id === 3;
-});
+const total = people.reduce((acc, currItem) => {
+  console.log(acc);
+  console.log(currItem);
 
-console.log(youngPerson);
+  return acc;
+}, 0);
