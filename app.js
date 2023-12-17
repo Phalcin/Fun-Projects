a23bac3041c83f8352ebed355828d31bdba5e2cd
phalcin <phalcinhayes@gmail.com>
2023-12-17 08:03:28 +0000

    new changes

--- app.js
+++ app.js
@@ -1,13 +1,1 @@
-const people = [
-  { name: 'bob', age: 20, position: 'developer', id: 1, salary: 200 },
-  { name: 'peter', age: 25, position: 'designer', id: 2, salary: 300 },
-  { name: 'suzy', age: 30, position: 'the boss', id: 3, salary: 500 },
-  { name: 'anna', age: 35, position: 'the boss', id: 4, salary: 500 },
-];
-
-const total = people.reduce((acc, currItem) => {
-  console.log(acc);
-  console.log(currItem);
-
-  return acc;
-}, 0);
+console.log(students);
---
+++ data.js
@@ -0,0 +1,32 @@
+const students = [
+  {
+    id: 1,
+    name: 'peter',
+    score: 80,
+    favoriteSubject: 'math',
+  },
+  {
+    id: 1,
+    name: 'Isaac',
+    score: 90,
+    favoriteSubject: 'history',
+  },
+  {
+    id: 1,
+    name: 'Alex',
+    score: 750,
+    favoriteSubject: 'art',
+  },
+  {
+    id: 1,
+    name: 'Rashid',
+    score: 85,
+    favoriteSubject: 'math',
+  },
+  {
+    id: 1,
+    name: 'Emmanuel',
+    score: 100,
+    favoriteSubject: 'math',
+  },
+];
