196515236e3707c7c093f8ed1549ff112aa08e35
phalcin <phalcinhayes@gmail.com>
2023-12-22 15:42:39 +0000

    new changes

--- faq-accordion-main/app.js
+++ faq-accordion-main/app.js
@@ -3,10 +3,21 @@ const minus = document.querySelectorAll('.icon__minus');
 const content = document.querySelectorAll('.item__content');
 
 const showContent = () => {
-  plus.forEach((plus, index) => {
-    plus.addEventListener('click', () => {
-      let showContent = content[index].classList.add('item__content--show');
-      let showMinus = minus[index].classList.add('icon__minus--show');
+  plus.forEach((pl, index) => {
+    pl.addEventListener('click', () => {
+      let itemShow = content[index];
+      let showContent = itemShow.classList.toggle('item__content--show');
+      if (showContent) {
+        pl.classList.toggle('icon__plus--hide');
+      }
+    });
+  });
+  minus.forEach((min, index) => {
+    min.addEventListener('click', () => {
+      let itemHide = content[index];
+      itemHide.classList.remove('item__content--show');
+
+      plus[index].classList.remove('icon__plus--hide');
     });
   });
 };
