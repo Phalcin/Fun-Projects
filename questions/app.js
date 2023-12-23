e3b319b6f5a07b44be67e6b15f7039cef7a9f705
phalcin <phalcinhayes@gmail.com>
2023-12-23 09:35:16 +0000

    new menu project

--- questions/app.js
+++ questions/app.js
@@ -7,7 +7,13 @@ questions.forEach((question) => {
   console.log(question);
   const btn = question.querySelector('.question-btn');
   btn.addEventListener('click', () => {
-    question;
+    questions.forEach((item) => {
+      if (item !== question) {
+        item.classList.remove('show-text');
+      }
+    });
+
+    question.classList.toggle('show-text');
   });
 });
 
