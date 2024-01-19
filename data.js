327ff033e69a5b29b52938e8a5939dad415d926d
phalcin <phalcinhayes@gmail.com>
2024-01-19 03:47:20 +0000

    new  changes

--- data.js
+++ data.js
@@ -1,32 +1,6 @@
-const students = [
-  {
-    id: 1,
-    name: 'peter',
-    score: 80,
-    favoriteSubject: 'math',
-  },
-  {
-    id: 1,
-    name: 'Isaac',
-    score: 90,
-    favoriteSubject: 'history',
-  },
-  {
-    id: 1,
-    name: 'Alex',
-    score: 75,
-    favoriteSubject: 'math',
-  },
-  {
-    id: 1,
-    name: 'Rashid',
-    score: 85,
-    favoriteSubject: 'art',
-  },
-  {
-    id: 1,
-    name: 'Emmanuel',
-    score: 100,
-    favoriteSubject: 'math',
-  },
-];
+// Arrays
+
+const myArray = ["A", "B", "C", "D", "E", "F"];
+
+const newArray = myArray.reverse;
+console.log(newArray);
