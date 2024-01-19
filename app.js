f3c8239749d4a3ba1c94839ff96cb50f18e6117f
phalcin <phalcinhayes@gmail.com>
2024-01-19 03:18:51 +0000

    new changes

--- app.js
+++ app.js
@@ -1,15 +1,42 @@
-const button1 = document.querySelector(".btn1");
-const button2 = document.querySelector(".btn2");
+// let playGame = confirm("Shall we play rock , paper or scissors?");
 
-function showThis() {
-  console.log(this);
-}
+// if (playGame) {
+//   // Play
+//   let playerChoice = prompt("Please enter rock, paper, or scissors.");
+//   if (playerChoice) {
+//     let playerOne = playerChoice.trim().toLowerCase();
+//     if (
+//       playerOne === "rock" ||
+//       playerOne === "paper" ||
+//       playerOne === "scissors"
+//     ) {
+//       let computerChoice = Math.floor(Math.random() * 3 + 1);
+//       let computer =
+//         computerChoice === 1
+//           ? "rock"
+//           : computerChoice === 2
+//           ? "paper"
+//           : "scissors";
 
-const emma = {
-  name: "emma",
-  showThis: showThis,
-};
-
-emma.showThis();
-
-button1.addEventListener("click", showThis);
+//       let result =
+//         playerOne === computer
+//           ? "Tie game!"
+//           : playerOne === "rock" && computer === "paper"
+//           ? `playerOne: ${playerOne}\nComputer: ${computer}\nComputer wins!`
+//           : playerOne === "paper" && computer === "scissors"
+//           ? `playerOne: ${playerOne}\nComputer: ${computer}\nComputer wins!`
+//           : playerOne === "scissors" && computer === "rock"
+//           ? `playerOne: ${playerOne}\nComputer: ${computer}\nComputer wins!`
+//           : `playerOne: ${playerOne}\nComputer: ${computer}\nplayerOne wins!`;
+//       alert(result);
+//       let playAgain = confirm("Play Again?");
+//       playAgain ? location.reload() : alert("Ok, thanks for playing.");
+//     } else {
+//       alert("You didn't enter rock, paper, or scissors.");
+//     }
+//   } else {
+//     alert("I guess you changed your mind. Maybe next time");
+//   }
+// } else {
+//   alert(`Ok, maybe next time.`);
+// }
