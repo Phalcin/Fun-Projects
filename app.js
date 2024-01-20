cc922e820590ea0a88719693c0bc05ca97e749bb
phalcin <phalcinhayes@gmail.com>
2024-01-20 21:44:30 +0000

    new changes

--- app.js
+++ app.js
@@ -1,43 +1,43 @@
-let playGame = confirm("Shall we play rock , paper or scissors?");
+// let playGame = confirm("Shall we play rock , paper or scissors?");
 
-if (playGame) {
-  while (playGame) {
-    // Play
-    const playerChoice = prompt("Please enter rock, paper, or scissors.");
-    if (playerChoice || playerChoice === "") {
-      let playerOne = playerChoice.trim().toLowerCase();
-      if (
-        playerOne === "rock" ||
-        playerOne === "paper" ||
-        playerOne === "scissors"
-      ) {
-        let computerChoice = Math.floor(Math.random() * 3);
-        const rpsArray = ["rock", "paper", "scissors"];
-        const computer = rpsArray[computerChoice];
+// if (playGame) {
+//   while (playGame) {
+//     // Play
+//     const playerChoice = prompt("Please enter rock, paper, or scissors.");
+//     if (playerChoice || playerChoice === "") {
+//       let playerOne = playerChoice.trim().toLowerCase();
+//       if (
+//         playerOne === "rock" ||
+//         playerOne === "paper" ||
+//         playerOne === "scissors"
+//       ) {
+//         let computerChoice = Math.floor(Math.random() * 3);
+//         const rpsArray = ["rock", "paper", "scissors"];
+//         const computer = rpsArray[computerChoice];
 
-        let result =
-          playerOne === computer
-            ? "Tie game!"
-            : playerOne === "rock" && computer === "paper"
-            ? `playerOne: ${playerOne}\nComputer: ${computer}\nComputer wins!`
-            : playerOne === "paper" && computer === "scissors"
-            ? `playerOne: ${playerOne}\nComputer: ${computer}\nComputer wins!`
-            : playerOne === "scissors" && computer === "rock"
-            ? `playerOne: ${playerOne}\nComputer: ${computer}\nComputer wins!`
-            : `playerOne: ${playerOne}\nComputer: ${computer}\nplayerOne wins!`;
-        alert(result);
-        playGame = confirm("Play Again?");
-        if (!playGame) alert("Ok thanks for playing.");
-        continue;
-      } else {
-        alert("You didn't enter rock, paper, or scissors.");
-        continue;
-      }
-    } else {
-      alert("I guess you changed your mind. Maybe next time");
-      break;
-    }
-  }
-} else {
-  alert("okay maybe next time");
-}
+//         let result =
+//           playerOne === computer
+//             ? "Tie game!"
+//             : playerOne === "rock" && computer === "paper"
+//             ? `playerOne: ${playerOne}\nComputer: ${computer}\nComputer wins!`
+//             : playerOne === "paper" && computer === "scissors"
+//             ? `playerOne: ${playerOne}\nComputer: ${computer}\nComputer wins!`
+//             : playerOne === "scissors" && computer === "rock"
+//             ? `playerOne: ${playerOne}\nComputer: ${computer}\nComputer wins!`
+//             : `playerOne: ${playerOne}\nComputer: ${computer}\nplayerOne wins!`;
+//         alert(result);
+//         playGame = confirm("Play Again?");
+//         if (!playGame) alert("Ok thanks for playing.");
+//         continue;
+//       } else {
+//         alert("You didn't enter rock, paper, or scissors.");
+//         continue;
+//       }
+//     } else {
+//       alert("I guess you changed your mind. Maybe next time");
+//       break;
+//     }
+//   }
+// } else {
+//   alert("okay maybe next time");
+// }
--- data.js
+++ data.js
@@ -1,6 +1,27 @@
-// Arrays
+// Javascript classes
 
-const myArray = ["A", "B", "C", "D", "E", "F"];
+class Pizza {
+  constructor(pizzaSize) {
+    this.size = pizzaSize;
+    this.crust = "original";
+  }
+  getCrust() {
+    return this.crust;
+  }
+  setCrust(pizzaCrust) {
+    return (this.crust = pizzaCrust);
+  }
+}
 
-const newArray = myArray.reverse;
-console.log(newArray);
+// class SpecialtyPizza extends Pizza {
+//   constructor(pizzaSize) {
+//     super(pizzaSize);
+//     this.type = "The Works";
+//   }
+//   slice() {
+//     console.log(`Our ${this.type} ${this.size} pizza has 8 slices`);
+//   }
+// }
+
+// const specialty = new SpecialtyPizza("medium");
+// specialty.slice();
