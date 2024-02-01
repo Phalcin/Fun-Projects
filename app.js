5ce05670d1474bca1243b651339400d4e568b129
phalcin <phalcinhayes@gmail.com>
2024-02-01 13:53:39 +0000

    learning python

--- app.js
+++ app.js
@@ -1,15 +1,5 @@
-function sum(numbers) {
-  "use Strict";
-  return numbers.reduce((acc, curr) => {
-    return acc + curr;
-  }, 0);
+function repeatStr(n, s) {
+  return s.repeat(n);
 }
 
-function past(h, m, s) {
-  h = 60;
-  m = 60;
-  s = 60;
-  return h * m * s;
-}
-
-console.log(past(0, 1, 1));
+console.log(repeatStr(6, "I"));
---
+++ app.py
@@ -0,0 +1,7 @@
+movie_rating = 8.5;
+
+# 1- Variable names must make sense
+
+# 2- Python convention is to use lowercase letters
+my_name = "Muslim Helalee"
+user__info
