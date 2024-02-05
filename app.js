4e37efb48541bf914f69429934fa4fad09ed9a0f
phalcin <phalcinhayes@gmail.com>
2024-02-05 04:40:37 +0000

    new changes

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
-function repeatStr(n, s) {
-  return s.repeat(n);
-}
+// function repeatStr(n, s) {
+//   return s.repeat(n);
+// }
 
-console.log(repeatStr(6, "I"));
+// console.log(repeatStr(6, "I"));
+
+let cars = "lamborghini";
+
+for (number of cars) {
+  console.log(number);
+}
--- app.py
+++ app.py
@@ -1,66 +1,34 @@
-# movie_name = "The good, bad"
-# # ordered
-# mixed = [1, 2, 3.2, 5.4, True, "Emmanuel", [1, 2, 3]]
-# print(type(mixed))
+import math
 
-# # unordered
-# user_info = {"user_name": "awesome", "user_id": 56}
-# console.log(user_info)
+random_local_girls = [
+    "Akua",
+    "Ama",
+    "Abena",
+    "Afia",
+    "Adwoa",
+    "Yaa",
+    "Akosua",
+]
 
-# tuple
-mixed_tuple = (
-    1,
-    2,
-    3,
-    4,
-    5,
-    True,
-    "Emmanuel",
-    [1, 2, 3, 4],
-)
-print(mixed_tuple)
-# unordered
-# set
 
-mixed_set = {1, 2, 3, 4, 5, True, "muslim "}
-print(mixed_set)
-# - len()
-address = "Ghana"
-print(len(address))
+def start_game():
+    return play_game()
 
-# 2 - [] Notation
-print(len(address))
 
-# 3 [] Notation
-print(address[0:4])
-print(address[-3:-1])
-print(address[:9])
-print(address[:])
+def play_game():
+    play_game = input("Do you wanna play the fucking game or not? ")
+    if play_game == "yes":
+        player_option = input("Type your freaking local Ghanaian name bro: ")
+        if player_option.__contains__("k"):
+            girl_name = random_local_girls[0]
+            print(f"Congratulations you match {girl_name}")
+    elif play_game == "no":
+        print("Go away bro, see ya next time")
+    elif play_game != "yes" or play_game != "no":
+        print("You should type only yes or no")
+        start_game()
+    else:
+        print("Bro we'll see you next fucking time")
 
-# 4 - Concatenation
-country = "USA"
-city = "NYC"
-full_address = city + ", " + country
-print(full_address)
 
-# 5 - upper()
-print(address.upper())
-print(address.lower())
-print(full_address.title())
-
-# 6 - strip()
-job = " programmer  "
-print(job)
-print(job.strip())
-
-# 7 - find()
-print(address.find("n"))
-print(address.replace("Gha", "Na"))
-
-# in operator
-print("ne" in address)
-print("na" in address)
-
-# 12 ! operator
-print("ne" not in address)
-print("na" not in address)
+play_game()
