b72489bdc0795fdb6f5453c7d35d84783a01e7a6
phalcin <phalcinhayes@gmail.com>
2024-02-10 10:17:56 +0000

    new changes

--- app.js
+++ app.js
@@ -1,20 +1,3 @@
-const counter = {
-  count: 0,
-  increment() {
-    console.log(this);
-    this.count++;
-    console.log(this.count);
-  },
-};
-
-const btn = document.querySelector(".increment");
-
-// fail
-// btn.addEventListener("click", counter.increment);
-
-// some edge cases
-// btn.addEventListener("click", counter.increment.bind(counter));
-
-const increment = counter.increment.bind(counter);
-btn.addEventListener("click", increment);
-btn.removeEventListener("click", increment);
+const firstName = "john";
+let lastName = "Jordan";
+var random = "random";
