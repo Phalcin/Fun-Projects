0b41f788e2b6962c4545689213a987380c7703b7
phalcin <phalcinhayes@gmail.com>
2024-02-12 05:32:48 +0000

    new changes

--- app.js
+++ app.js
@@ -1,24 +1,17 @@
-const heading1 = document.querySelector(".one");
-const heading2 = document.querySelector(".two");
-const heading3 = document.querySelector(".three");
-const container = document.querySelector(".img-container");
-const btn = document.querySelector(".btn");
+const xhr = new XMLHttpRequest();
+console.log(xhr);
 
-btn.addEventListener("click", async () => {
-  await addColor(1000, heading1, "red");
-  await addColor(1000, heading2, "green");
-  await addColor(1000, heading3, "blue");
-});
-
-function addColor(time, element, color) {
-  return new Promise((resolve, reject) => {
-    if (element) {
-      setTimeout(() => {
-        element.style.color = color;
-        resolve();
-      }, time);
-    } else {
-      reject(new Error(`There is no such element ${element}`));
-    }
-  });
-}
+xhr.open("GET", "./apptext.txt");
+xhr.onreadystatechange = function () {
+  if (xhr.readyState === 4 && xhr.status === 200) {
+    const text = document.createElement("p");
+    text.textContent = xhr.responseText;
+    document.body.appendChild(text);
+  } else {
+    console.log({
+      status: xhr.status,
+      text: xhr.statusText,
+    });
+  }
+};
+xhr.send();
