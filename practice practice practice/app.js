9f8996210cd9d164ccca99e49fc7e445539afadf
phalcin <phalcinhayes@gmail.com>
2024-03-01 00:01:12 +0000

    new changes

--- practice practice practice/app.js
+++ practice practice practice/app.js
@@ -1,33 +1,33 @@
-function randomNumber(min, max) {
-  return Math.floor(Math.random() * (max - min + 1) + min);
-}
+// function randomNumber(min, max) {
+//   return Math.floor(Math.random() * (max - min + 1) + min);
+// }
 
-function roll() {
-  let minValue = 1;
-  let maxValue = 6;
-  let roll = randomNumber(minValue, maxValue);
-  return roll;
-}
+// function roll() {
+//   let minValue = 1;
+//   let maxValue = 6;
+//   let roll = randomNumber(minValue, maxValue);
+//   return roll;
+// }
 
-while (true) {
-  let players = prompt("Enter the number of players (2-4) : ");
+// while (true) {
+//   let players = prompt("Enter the number of players (2-4) : ");
 
-  if (players === "q") {
-    break;
-  }
-  if (parseFloat(players)) {
-    if (players >= 2 && players <= 4) {
-      alert(players);
-      break;
-    } else {
-      alert("Must be between 2-4 players.");
-    }
-  } else {
-    alert("Invalid try again.");
-  }
-  max_score = 50;
+//   if (players === "q") {
+//     break;
+//   }
+//   if (parseFloat(players)) {
+//     if (players >= 2 && players <= 4) {
+//       alert(players);
+//       break;
+//     } else {
+//       alert("Must be between 2-4 players.");
+//     }
+//   } else {
+//     alert("Invalid try again.");
+//   }
+//   max_score = 50;
 
-  let player_scores = players.map((player) => {
-    console.log(players);
-  });
-}
+//   let player_scores = players.map((player) => {
+//     console.log(players);
+//   });
+// }
