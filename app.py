4a476ca1ab09a49cdb43c073ef9aa14ddcaa3e91
phalcin <phalcinhayes@gmail.com>
2024-03-01 00:08:42 +0000

    new changes

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ employee_hourly_pay = float(input("Enter the hourly pay"))
 employee_gross_pay = employee_hourly_pay * 40
 overtime_wages = employee_hourly_pay * 1.5
 
+print(employee_hourly_pay)
+
 
 # You can change the variable names to whatever you want, just make sure it matches what the question is about.
 #  Don't use AI much if you're a beginner else you'll suck at coding :)
