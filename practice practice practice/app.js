15f9717e24c747c44ab46653f7aa57ef10e97818
phalcin <phalcinhayes@gmail.com>
2024-03-10 04:40:36 +0000

    new changes

--- practice practice practice/app.js
+++ practice practice practice/app.js
@@ -1,44 +1,27 @@
-// const intervalId = setInterval(() => {
-//   console.log("well this will run for quite a while");
-// }, 2000);
+const panels = document.querySelectorAll(".panel");
+const header = document.querySelector("h3");
+const container = document.querySelectorAll(".container");
 
-// setTimeout(function () {
-//   clearInterval(intervalId);
-//   console.log(`interval stopped`);
-// }, 3000);
-
-// const foo = ["one", "two"];
-
-// const [red, yellow] = foo;
-// console.log(red, yellow);
-
-// function f() {
-//   return [1, 2];
+// for (let i = 0; i < panel.length; i++) {
+//   let currentImg = panel[i];
+//   currentImg.addEventListener("click", () => {
+//     if (currentImg.classList.contains("active")) {
+//       currentImg.classList.remove("active");
+//     } else {
+//       currentImg.classList.toggle("active");
+//     }
+//   });
 // }
 
-// let [c, d] = f();
-// console.log(c, d);
+panels.forEach((panel) => {
+  panel.addEventListener("click", () => {
+    removeActiveClasses();
+    panel.classList.toggle("active");
+  });
+});
 
-// const [a, ...b] = ["one", "two", "three"];
-// console.log(a, b);
-
-// const person = {
-//   age: 20,
-//   name: "Emmanuel",
-//   others: ["well", "well", 3, 4, 5],
-// };
-
-// function emma({ age, name, others: [one, two] }) {
-//   return `My name is ${name}, I am ${age} years old, other things like ${one} ${two}`;
-// }
-
-// console.log(emma(person));
-
-const songs = [
-  { name: "Lucky You", singer: "Joyner", duration: 4.34 },
-  { name: "just like you", singer: "NF", duration: 3.23 },
-  { name: "Humble", singer: "Kendrick", duration: 2.33 },
-];
-
-const [, , { singer: s }] = songs;
-console.log(s);
+function removeActiveClasses() {
+  panels.forEach((panel) => {
+    panel.classList.remove("active");
+  });
+}
