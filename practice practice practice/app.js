1040ab0d905bdab464e19895fbfd42619353cfdc
phalcin <phalcinhayes@gmail.com>
2024-03-10 01:26:39 +0000

    new changes

--- practice practice practice/app.js
+++ practice practice practice/app.js
@@ -1,8 +1,13 @@
-const intervalId = setInterval(() => {
-  console.log("well this will run for quite a while");
-}, 2000);
+// const intervalId = setInterval(() => {
+//   console.log("well this will run for quite a while");
+// }, 2000);
 
-setTimeout(function () {
-  clearInterval(intervalId);
-  console.log(`interval stopped`);
-}, 3000);
+// setTimeout(function () {
+//   clearInterval(intervalId);
+//   console.log(`interval stopped`);
+// }, 3000);
+
+const foo = ["one", "two"];
+
+const [red, yellow] = foo;
+console.log(red, yellow);
