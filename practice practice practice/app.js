ce1b5032906254eda459b4ec83025fc75781879a
phalcin <phalcinhayes@gmail.com>
2024-03-10 01:18:27 +0000

    new changes

--- practice practice practice/app.js
+++ practice practice practice/app.js
@@ -1,33 +1,8 @@
-// function randomNumber(min, max) {
-//   return Math.floor(Math.random() * (max - min + 1) + min);
-// }
+const intervalId = setInterval(() => {
+  console.log("well this will run for quite a while");
+}, 2000);
 
-// function roll() {
-//   let minValue = 1;
-//   let maxValue = 6;
-//   let roll = randomNumber(minValue, maxValue);
-//   return roll;
-// }
-
-// while (true) {
-//   let players = prompt("Enter the number of players (2-4) : ");
-
-//   if (players === "q") {
-//     break;
-//   }
-//   if (parseFloat(players)) {
-//     if (players >= 2 && players <= 4) {
-//       alert(players);
-//       break;
-//     } else {
-//       alert("Must be between 2-4 players.");
-//     }
-//   } else {
-//     alert("Invalid try again.");
-//   }
-//   max_score = 50;
-
-//   let player_scores = players.map((player) => {
-//     console.log(players);
-//   });
-// }
+setTimeout(function () {
+  clearInterval(intervalId);
+  console.log(`interval stopped`);
+}, 3000);
