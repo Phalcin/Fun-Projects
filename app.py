88dcb4e4d58c306ddd250d1b20c93ee28cfe3160
Phalcin <phalcinhayes@gmail.com>
2024-03-21 05:36:58 +0000

    new changes

--- app.py
+++ app.py
@@ -1,20 +1,33 @@
-# number_of_seconds = float(input('Enter the number of seconds: '))
+user_wins = 0
+computer_wins = 0
 
-# one_mi
+options = ["rock", "paper", "scissors"]
 
-# if number_of_seconds > 60:
+while True:
+    user_input = input("Type Rock/Paper/Scissors or Q to quit: ").lower()
+    if user_input == "q":
+        break
 
+    if user_input not in options:
+        continue
 
-employee_hourly_pay = float(input("Enter the employee hourly wage: Ghs"))
+    random_number = random.randint(0, 2)
+    # rock : 0, paper: 1, scissors: 2
+    computer_pick = options[random_number]
 
-employee_work_hours = float(input("Enter the employee work time: "))
+    if user_input == "rock" and computer_pick == "scissors":
+        print("You won!")
+        user_wins += 1
 
-if employee_work_hours <= 40:
-    employee_regular_pay = employee_hourly_pay * employee_work_hours
-    print(f"Employees pay for the week is: Ghs{employee_regular_pay}")
-else:
-    employee_regular_pay = employee_hourly_pay * 40
-    overtime_pay = (employee_work_hours - 40) * employee_hourly_pay * 1.5
-    print(
-        f"Employee's total pay is Ghs{employee_regular_pay + overtime_pay}, including overtime pay of Ghs{overtime_pay}"
-    )
+    elif user_input == "paper" and computer_pick == "rock":
+        print("you won!")
+        user_wins += 1
+    elif user_input == "scissors" and computer_pick == "paper":
+        print("You won!")
+        user_wins += 1
+    else:
+        print("You lost!")
+        computer_wins += 1
+
+    print("You won", user_wins, "times.")
+    print("the computer won", computer_wins, "times")
